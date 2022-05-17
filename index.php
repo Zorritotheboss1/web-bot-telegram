4d345f832afe1520b09ae62007e7fb0b07171352
Zorritotheboss1 <104961755+Zorritotheboss1@users.noreply.github.com>
2022-05-17 05:59:06 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <!-- celulares -->
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!--bosstraps 5--><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link href="boos/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <meta charset="utf-8">
+<!-- titulo de la pagina -->
+    <title>Login</title>
+<!-- tipo de fuente -->
+   <link href="https://fonts.googleapis.com/css2?family=Roboto+Flex:opsz,wght@8..144,200;8..144,400;8..144,700&display=swap" rel="stylesheet"><title>Bienvenidos a la pag donde encotraras todos los codigos gratis que necesites</title>
+  <!-- css de la pagina -->
+    <link rel="stylesheet" href="assets/css/index.css">
+  </head>
+  <body>
+    <!-- titulo de estilo de la pagina -->
+<header>
+<h1>Esta pagina consiste en que usted descargue y aprenda a montar los bots de descargas gratuitas asi no gasta un peso en comprar alguno</h1>
+</header>
+ <h1>Iniciar Sesión</h1>
+      
+    <form action="login.php" method="post">
+      <input type="submit" name="" value="Login">
+    </form>
+
+
+    <form action="singup.php" method="post">
+    <h1>Registrese</h1>
+    <input type="submit" name="" value="Registrese">
+
+    </form>
+
+
+
+</body>
+ </html>
---
+++ singup.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Registro</title>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Flex:opsz,wght@8..144,200;8..144,400;8..144,700&display=swap" rel="stylesheet"><title>Bienvenidos a la pag donde encotraras todos los codigos gratis que necesites</title>
+    <link rel="stylesheet" href="assets/css/registro.css">
+  </head>
+  <body>
+
+ <h1>Crea tu cuenta</h1>
+
+    <form action="login.php" method="post">
+      <input type="submit" name="" value="Login">
+    </form>
+
+
+    <form action="singup.php" method="post">
+    <h1>Registrese</h1>
+    <input type="submit" name="" value="Registrese">
+
+    </form>
+
+
+
+</body>
+ </html>
