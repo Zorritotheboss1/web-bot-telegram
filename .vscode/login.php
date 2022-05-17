bfae59faa63900b40cb13a781866785fe48f76a6
Zorritotheboss1 <104961755+Zorritotheboss1@users.noreply.github.com>
2022-05-17 05:56:25 +0200

    Add files via upload

---
+++ .vscode/login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Login</title>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Flex:opsz,wght@8..144,200;8..144,400;8..144,700&display=swap" rel="stylesheet"><title>Bienvenidos a la pag donde encotraras todos los codigos gratis que necesites</title>
+    <link rel="stylesheet" href="assets/css/login.css">
+
+  </head>
+  <body>
+<header>
+
+</header>
+    <h1>Login</h1>
+<form action="login.php" method="post">
+  <input type="text" name="Nombre" placeholde="Ingrese su nombre">
+  <input type="password" name="Password" placeholde="Ingrese su password">
+  <input type="submit" name="" value="Enviar">
+</form>
+  </body>
+</html>
